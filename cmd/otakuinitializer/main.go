2ee6edfa38c6a47ee79e0ee64cebace55ca1e275
Weit <caroxmeme@gmail.com>
2025-10-27 00:03:59 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/otakuinitializer/main.go
@@ -0,0 +1,23 @@
+// cmd/otakuinitializer/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "otakuinitializer/internal/otakuinitializer"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := otakuinitializer.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
